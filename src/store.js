f940bc2e9b4ff67036d97ba557b915ecc680c096
mastro-elfo <francesco.209@gmail.com>
2021-06-19 20:12:48 +0200

    Add login to app store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import hasAdmin from "./features/hasAdmin";
+import login from "./features/login";
 
-export default configureStore({ reducer: { hasAdmin } });
+export default configureStore({ reducer: { hasAdmin, login } });
