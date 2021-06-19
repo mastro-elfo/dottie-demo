9c017e0b1daa28c927dbfdc3e924f8fa7f426016
mastro-elfo <francesco.209@gmail.com>
2021-06-19 20:12:17 +0200

    Create login reducer

--- src/features/login.js
+++ src/features/login.js
@@ -1,18 +1,48 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-const fetchUserById = createAsyncThunk(
-  "users/fetchByIdStatus",
-  async (userId, thunkAPI) => {
-    const response = await userAPI.fetchById(userId);
-    return response.data;
+import { readAll } from "../controllers/user";
+import { encrypt } from "../controllers/utils";
+import { UserNotFound } from "../errors/";
+
+export const login = createAsyncThunk(
+  "login/login",
+  async ({ username, password }) => {
+    const encrypted = encrypt(password);
+    return readAll()
+      .then((list) =>
+        list.find((u) => u.username === username && u.password === encrypted)
+      )
+      .then((user) => {
+        if (user) {
+          return user;
+        }
+        throw new UserNotFound();
+      });
   }
 );
 
-const login = createSlice({
+const loginSlice = createSlice({
   name: "login",
   initialState: {
+    loading: false,
+    error: null,
     logged: null,
-    hasAdmin: false,
   },
-  extraReducers: {},
+  extraReducers: {
+    [login.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.error = false;
+      state.logged = payload;
+    },
+    [login.pending]: (state, { payload }) => {
+      state.loading = true;
+      state.error = false;
+    },
+    [login.rejected]: (state, { error }) => {
+      state.loading = false;
+      state.error = error.name;
+    },
+  },
 });
+
+export default loginSlice.reducer;
