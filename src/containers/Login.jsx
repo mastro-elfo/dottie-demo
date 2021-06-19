2ddfdb5711d0f326da9f3d1f316d796249588de7
mastro-elfo <francesco.209@gmail.com>
2021-06-19 20:11:42 +0200

    Create login functionality

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,19 +1,25 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, TextField } from "@material-ui/core";
+import { Box, Button, TextField, Typography } from "@material-ui/core";
 import { Content, Header, Page } from "mastro-elfo-mui";
 import { Responsive } from "../components";
 
-import { login } from "../features/users";
+import { login } from "../features/login";
 
 export default function Component({ children }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
-  const logged = useSelector((state) => state.users.logged);
+  const { loading, logged, error } = useSelector((state) => state.login);
 
   const handleLogin = () => {
     dispatch(login({ username, password }));
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleLogin();
+    }
   };
 
   if (!logged) {
@@ -28,11 +34,17 @@ export default function Component({ children }) {
               md={10}
               xl={8}
             >
+              {!!error && (
+                <Typography color="error" gutterBottom>
+                  {error}
+                </Typography>
+              )}
               <TextField
                 fullWidth
                 label="Username"
                 value={username}
                 onChange={({ target: { value } }) => setUsername(value)}
+                onKeyPress={handleKeyPress}
               />
               <TextField
                 fullWidth
@@ -40,15 +52,18 @@ export default function Component({ children }) {
                 type="password"
                 value={password}
                 onChange={({ target: { value } }) => setPassword(value)}
+                onKeyPress={handleKeyPress}
               />
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleLogin}
-                disabled={!username || !password}
-              >
-                Login
-              </Button>
+              <Box py={1}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleLogin}
+                  disabled={!username || !password || loading}
+                >
+                  Login
+                </Button>
+              </Box>
             </Responsive>
           </Content>
         }
