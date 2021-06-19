556be707eeb13783cb09946a7284681dfe2dd522
mastro-elfo <francesco.209@gmail.com>
2021-06-19 22:30:08 +0200

    Improve Responsive component

--- src/components/Responsive.jsx
+++ src/components/Responsive.jsx
@@ -1,20 +1,40 @@
+import PropTypes from "prop-types";
+
 import { Grid } from "@material-ui/core";
 
 export default function Responsive({
   children,
-  xs = false,
-  sm = false,
-  md = false,
-  lg = false,
-  xl = false,
+  Container = DefaultContainer,
   ContainerProps = {},
-  ItemProps = {},
+  Item = DefaultItem,
+  ...rest
 }) {
   return (
-    <Grid container {...ContainerProps}>
-      <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl} {...ItemProps}>
-        {children}
-      </Grid>
+    <Container {...ContainerProps}>
+      <Item {...rest}>{children}</Item>
+    </Container>
+  );
+}
+
+function DefaultContainer({ children, ...props }) {
+  return (
+    <Grid container {...props}>
+      {children}
     </Grid>
   );
 }
+
+function DefaultItem({ children, ...props }) {
+  return (
+    <Grid item {...props}>
+      {children}
+    </Grid>
+  );
+}
+
+Responsive.propTypes = {
+  children: PropTypes.node,
+  Container: PropTypes.node,
+  ContainerProps: PropTypes.object,
+  Item: PropTypes.node,
+};
