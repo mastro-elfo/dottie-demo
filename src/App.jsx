3f234dfcddc294892e81d8d3339c20d1b16c18c8
mastro-elfo <francesco.209@gmail.com>
2021-06-22 00:23:18 +0200

    Fix basename

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,7 @@ export default function App() {
       }}
       RouterProps={{
         Router: BrowserRouter,
+        basename: "dottie-demo",
         routes: [
           about,
           log,
