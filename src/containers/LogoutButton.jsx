21e54caef89da8cb9c831d48e44f5f9c10c581cc
mastro-elfo <francesco.209@gmail.com>
2021-06-21 11:35:34 +0200

    Add Logout success snackbar

--- src/containers/LogoutButton.jsx
+++ src/containers/LogoutButton.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 
 import { IconButton } from "@material-ui/core";
+import { useSnackbar } from "notistack";
 
 import LogoutIcon from "@material-ui/icons/ExitToApp";
 
@@ -9,14 +10,17 @@ import { logout } from "../features/login";
 export default function LogoutButton() {
   const dispatch = useDispatch();
   const { loading } = useSelector((state) => state.login);
+  const { enqueueSnackbar } = useSnackbar();
 
   const handleLogout = () => {
     dispatch(logout())
       .then(() => {
         console.log("Logout successful");
+        enqueueSnackbar("Logout successful", { variant: "success" });
       })
       .catch((err) => {
         console.error("Logout Error", err);
+        enqueueSnackbar("Logout Error", { variant: "error" });
       });
   };
   return (
