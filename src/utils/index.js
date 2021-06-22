681b4de662fa009595ae1794f439b45ae23d12b2
mastro-elfo <francesco.209@gmail.com>
2021-06-22 23:56:00 +0200

    Add `toLocaleString` utility

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+export function toLocaleString(date) {
+  return date.toLocaleString(undefined, {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
